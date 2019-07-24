93c12083863577948a29302245ab84b5cc5fff42
jpimentel45 <jpimentel45@gmail.com>
2019-07-23 19:38:35 -0700

    new mongo uri

--- onionScrapper/app.js
+++ onionScrapper/app.js
@@ -18,7 +18,12 @@ app.set('view engine', 'hbs');
 //hbs.registerPartials(__dirname + '/views/partials');
 
 //set up mongoose
-mongoose.connect('mongodb://localhost/scraped');
+
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
