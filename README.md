85a49e12f7e1e9d428b76cd29ac75a4e9728025b
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-07-27 15:14:12 -0700

    mongo, views, routes working, need to pull more data from onion, prevent multiple data in mongo

---
+++ README.md
@@ -0,0 +1,10 @@
+# [Scrapping Onions](https://jpimentel45.github.io/Portfolio/)
+
+##About
+The Onion scrapper uses axios to make a call to the provided link, render the returned html based on what is needed. User should be able to scrape, make comments on each article scrapped, save and update comments
+
+##Techologies Used:
+
+1. Mongo DB
+2. jQuery
+3. HTML
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+const hbs = require("express-handlebars");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Require all models
+const db = require("./models");
+const articleRouter = require("./controllers/article-controller");
+const PORT = 3000;
+
+// Initialize Express
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultView: "default",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials"
+  })
+);
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+app.use("/", articleRouter);
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", {
+  useNewUrlParser: true
+});
+
+// Routes
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
